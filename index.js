2d77149dfe1ce246944ab643a316a40a34a0a263
Michael Clarke <bspride11@gmail.com>
2016-09-20 00:01:57 -0400

    Remove error handlers

--- index.js
+++ index.js
@@ -19,37 +19,6 @@ var pool = new Pool(config);
 var app = express();
 app.set('port', (process.env.PORT || 5000));
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
-
-// error handlers
-
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
-    });
-  });
-}
-
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
-});
-
 app.get('/', function(req, res) {
 	var page = "<h1>Test</h1>";
 	res.send(page)
