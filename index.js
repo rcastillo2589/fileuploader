31f0d47a699edabb0aabb921e5011bab1f0f4453
Michael Clarke <bspride11@gmail.com>
2016-09-19 23:54:55 -0400

    Convert index.js to api

--- index.js
+++ index.js
@@ -1,29 +1,23 @@
 var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+var url = require('url');
+var Pool = require('pg').Pool;
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
+var params = url.parse(process.env.DATABASE_URL);
+var auth = params.auth.split(':');
+
+var config = {
+  user: auth[0],
+  password: auth[1],
+  host: params.hostname,
+  port: params.port,
+  database: params.pathname.split('/')[1],
+  ssl: true
+};
+
+var pool = new Pool(config);
 
 var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', routes);
-app.use('/users', users);
+app.set('port', (process.env.PORT || 5000));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -56,5 +50,11 @@ app.use(function(err, req, res, next) {
   });
 });
 
+app.get('/', function(req, res) {
+	var page = "<h1>Test</h1>";
+	res.send(page)
+});
 
-module.exports = app;
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
