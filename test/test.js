3c383b6a87469768e06bff0745ab2c38e8256a94
Michael Clarke <bspride11@gmail.com>
2016-09-22 01:02:25 -0400

    Modify test to test express routes

--- test/test.js
+++ test/test.js
@@ -1,6 +1,9 @@
 var chai = require('chai');
+var chaiHttp = require('chai-http');
 var assert = chai.assert,
     foo = 'bar';
+var should = chai.should();
+chai.Use(chaiHttp);
 
 describe('foo', function() {
   describe('', function() {
@@ -8,4 +11,31 @@ describe('foo', function() {
       assert.typeOf(foo, 'string');
     });
   });
+});
+
+describe('file uploader api', function() {
+  var server;
+  beforeEach(function () {
+    delete require.cache[require.resolve('../index')];
+    server = require('../index');
+  });
+  afterEach(function (done) {
+    server.close(done);
+  });
+  it('responds to /', function testSlash(done) {
+    chai.request(server)
+      .get('/')
+      .end(function(err, res) {
+        res.should.have.status(200);
+        done();
+      });
+  });
+  it('404 everything else', function testPath(done) {
+    chai.request(server)
+      .get('/foo/bar')
+      .end(function(err, res) {
+        res.should.have.status(404);
+        done();
+      })
+  })
 });
